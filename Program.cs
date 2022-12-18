1598577a04d2f7c8a791e0e87097aa6245c91861
Othon <programmingxsup@gmail.com>
2022-12-18 18:01:57 -0300

    funcao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿
+decimal funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado)
+{
+  return (totalNegociado + 10) * porcentagem;
+}
 
+Console.WriteLine(funcaoRetornaQuantitadePOV(0.1M, 90));
+Console.WriteLine(funcaoRetornaQuantitadePOV(0.1M, 100));
+Console.WriteLine(funcaoRetornaQuantitadePOV(0.2M, 70));
