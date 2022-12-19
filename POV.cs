d144c4681b32c05b75653858d9a9bd766d63af97
Othon <programmingxsup@gmail.com>
2022-12-19 20:26:12 -0300

    Creating class

---
+++ POV.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ADA_exercicios_lista1_parte2
+{
+  public static class POV
+  {
+    public static decimal FuncaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado)
+    {
+      return (totalNegociado + 10) * porcentagem;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
-﻿
-decimal funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado)
-{
-  return (totalNegociado + 10) * porcentagem;
-}
+﻿using ADA_exercicios_lista1_parte2;
 
-Console.WriteLine(funcaoRetornaQuantitadePOV(0.1M, 90));
-Console.WriteLine(funcaoRetornaQuantitadePOV(0.1M, 100));
-Console.WriteLine(funcaoRetornaQuantitadePOV(0.2M, 70));
+Console.WriteLine(POV.FuncaoRetornaQuantitadePOV(0.1M, 90));
+Console.WriteLine(POV.FuncaoRetornaQuantitadePOV(0.1M, 100));
+Console.WriteLine(POV.FuncaoRetornaQuantitadePOV(0.2M, 70));
